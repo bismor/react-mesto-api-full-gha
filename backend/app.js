f34c09d307bac997fff45e75497f0952595cdc46
Vladislav Kosyachenko <cwat2011@yandex.ru>
2023-05-08 17:03:27 +0300

    Краш-тест сервера

--- backend/app.js
+++ backend/app.js
@@ -37,6 +37,12 @@ app.use((req, res, next) => {
   }
 });
 
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 app.post('/signin', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().regex(/^\S+@\S+\.\S+$/),
